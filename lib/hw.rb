97a073a161156dedba607bbb9c99a1afcc104c10
naofumi-fujii <naofumi.fujii@gmail.com>
2018-06-07 14:23:41 +0900

    simple logic

--- lib/hw.rb
+++ lib/hw.rb
@@ -1,5 +1,18 @@
 require "hw/version"
+require "thor"
 
 module Hw
   # Your code goes here...
+  class Cli < Thor
+    method_option :word, type: :string, required: true
+
+    desc "hello", "prints something"
+    def hello
+      if options[:word]
+        puts "hello #{options[:word]}"
+      else
+        puts "hello world"
+      end
+    end
+  end
 end
